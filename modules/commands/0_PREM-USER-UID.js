f808ce05766aa4940128b85c0c95a7388988cfe1
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-09-18 21:52:31 +0530

    Update 0_PREM-USER-UID.js

--- modules/commands/0_PREM-USER-UID.js
+++ modules/commands/0_PREM-USER-UID.js
@@ -4,14 +4,23 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "PREM BABU",
 	description: "THIS BOT WAS MADE BY MR PREM BABU",
-	commandCategory: "USER UID NUMBER",
-	cooldowns: 5
+	commandCategory: "USER UID",
+	cooldowns: 0
 };
 
-module.exports.run = function({ api, event }) {
-	if (Object.keys(event.mentions) == 0) return api.sendMessage(`${event.senderID}`, event.threadID, event.messageID);
-	else {
+module.exports.run = async function({ api, event, args }) {
+    const axios = global.nodemodule['axios'];
+    if(event.type == "message_reply") {
+	uid = event.messageReply.senderID
+	return api.sendMessage(`${uid}`, event.threadID, event.messageID) }
+    if (!args[0]) {return api.sendMessage(`${event.senderID}`, event.threadID, event.messageID);}
+    else {
+	if (args[0].indexOf(".com/")!==-1) {
+    const res_ID = await api.getUID(args[0]);  
+    return api.sendMessage(`${res_ID}`, event.threadID, event.messageID) }
+    else {
 		for (var i = 0; i < Object.keys(event.mentions).length; i++) api.sendMessage(`${Object.values(event.mentions)[i].replace('@', '')}: ${Object.keys(event.mentions)[i]}`, event.threadID);
 		return;
 	}
+}
 }
