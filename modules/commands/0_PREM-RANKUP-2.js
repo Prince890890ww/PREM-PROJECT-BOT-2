7aa26c672d2527d73afbc15e69aaf861480d1fb4
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-04 20:50:18 +0530

    Update 0_PREM-RANKUP-2.js

--- modules/commands/0_PREM-RANKUP-2.js
+++ modules/commands/0_PREM-RANKUP-2.js
@@ -13,15 +13,11 @@ module.exports.config = {
 
 module.exports.handleEvent = async function({ api, event, Currencies, Users, getText }) {
 	var {threadID, senderID } = event;
-	const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-  const { loadImage, createCanvas } = require("canvas");
-  const fs = global.nodemodule["fs-extra"];
+	const { createReadStream } = global.nodemodule["fs-extra"];
   const axios = global.nodemodule["axios"];
+  const fs = global.nodemodule["fs-extra"];
   let pathImg = __dirname + "/PREM-BABU/RANKUP/rankup.png";
-  let pathAvt1 = __dirname + "/cache/Avtmot.png";
-  var id1 = event.senderID;
   
-
 	threadID = String(threadID);
 	senderID = String(senderID);
 
@@ -41,64 +37,31 @@ module.exports.handleEvent = async function({ api, event, Currencies, Users, get
 	const level = Math.floor((Math.sqrt(1 + (4 * (exp + 1) / 3) + 1) / 2));
 
 	if (level > curLevel && level != 1) {
-		const name = global.data.userName.get(senderID) || await Users.getNameUser(senderID);
-		var messsage = (typeof thread.customRankup == "undefined") ? msg = getText("levelup") : msg = thread.customRankup, 
-			arrayContent;
+		var memes = [
+      "https://i.imgur.com/funny1.jpeg",
+      "https://i.imgur.com/funny2.jpeg",
+      "https://i.imgur.com/funny3.jpeg",
+      "https://i.imgur.com/funny4.jpeg",
+      "https://i.imgur.com/funny5.jpeg",
+      "https://i.imgur.com/funny6.jpeg",
+      "https://i.imgur.com/funny7.jpeg",
+      "https://i.imgur.com/funny8.jpeg",
+      "https://i.imgur.com/funny9.jpeg",
+      "https://i.imgur.com/funny10.jpeg"
+    ];
+    var randomMeme = memes[Math.floor(Math.random() * memes.length)];
 
-		messsage = messsage
-			.replace(/\{name}/g, name)
-			.replace(/\{level}/g, level);
+    // Download the meme image
+    let getMemeImage = (
+      await axios.get(`${randomMeme}`, {
+        responseType: "arraybuffer",
+      })
+    ).data;
+    fs.writeFileSync(pathImg, Buffer.from(getMemeImage, "utf-8"));
 
-		const moduleName = this.config.name;
-
-    var background = [
-  "https://i.imgur.com/aCyF3im.jpeg",
-  "https://i.imgur.com/sRJ32Kj.jpeg",
-  "https://i.imgur.com/zWHQKMK.jpeg",
-  "https://i.imgur.com/y7eGdkx.jpeg",
-  "https://i.imgur.com/jGIEcp8.jpeg",
-  "https://i.imgur.com/rgDPN4P.jpeg",
-  "https://i.imgur.com/xsZcwBg.jpeg",
-  "https://i.imgur.com/a1Cyidj.jpeg",
-  "https://i.imgur.com/3tILAYo.jpeg",
-  "https://i.imgur.com/wkIU5tG.jpeg",
-  "https://i.imgur.com/0EZi8lp.jpeg",
-  "https://i.imgur.com/UgVht2B.jpeg",
-  "https://i.imgur.com/1DPkMLj.jpeg",
-  "https://i.imgur.com/yAvH0zR.jpeg",
-  "https://i.imgur.com/4BLBnp3.jpeg",
-  "https://i.imgur.com/10iTZTP.jpeg",
-  "https://i.imgur.com/EjjUfiR.jpeg"  
-  ];
-    var rd = background[Math.floor(Math.random() * background.length)];
-    let getAvtmot = (
-    await axios.get(
-      `https://graph.facebook.com/${id1}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
-      { responseType: "arraybuffer" }
-    )
-  ).data;
-  fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
-  
-  let getbackground = (
-    await axios.get(`${rd}`, {
-      responseType: "arraybuffer",
-    })
-  ).data;
-  fs.writeFileSync(pathImg, Buffer.from(getbackground, "utf-8"));
-  
-    let baseImage = await loadImage(pathImg);
-    let baseAvt1 = await loadImage(pathAvt1);
-    let canvas = createCanvas(baseImage.width, baseImage.height);
-    let ctx = canvas.getContext("2d");
-    ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-    ctx.rotate(-0 * Math.PI / 180);
-    ctx.drawImage(baseAvt1, 458, 541, 1005, 1005);
-    const imageBuffer = canvas.toBuffer();
-    fs.writeFileSync(pathImg, imageBuffer);
-    fs.removeSync(pathAvt1);
-		api.sendMessage({body: messsage, mentions: [{ tag: name, id: senderID }], attachment: fs.createReadStream(pathImg) }, event.threadID, () => fs.unlinkSync(pathImg));
-    
-}
+    // Send the meme image
+		api.sendMessage({attachment: fs.createReadStream(pathImg)}, event.threadID, () => fs.unlinkSync(pathImg));
+	}
 
 	await Currencies.setData(senderID, { exp });
 	return;
@@ -108,8 +71,7 @@ module.exports.languages = {
 	"en": {
 		"on": "on",
 		"off": "off",
-		"successText": "success notification rankup!",
-		"levelup": "◦•●◉✿ दिल की बात ✿◉●•◦",
+		"successText": "success notification rankup!"
 	}
 }
 
@@ -123,4 +85,4 @@ module.exports.run = async function({ api, event, Threads, getText }) {
 	await Threads.setData(threadID, { data });
 	global.data.threadData.set(threadID, data);
 	return api.sendMessage(`${(data["rankup"] == true) ? getText("on") : getText("off")} ${getText("successText")}`, threadID, messageID);
-                            }
+}
