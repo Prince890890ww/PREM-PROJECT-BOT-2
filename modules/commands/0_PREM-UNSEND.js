2f9f4f82bac3205b4c3c6b41b9cfae842befb11c
prem-project3608 <144128294+prem-project3608@users.noreply.github.com>
2024-10-08 22:06:04 +0530

    Update 0_PREM-UNSEND.js

--- modules/commands/0_PREM-UNSEND.js
+++ modules/commands/0_PREM-UNSEND.js
@@ -12,12 +12,23 @@ module.exports.config = {
 module.exports.languages = {
 	"en": {
 		"returnCant": "किसी और का मैसेज मैं कैसे डिलीट करूं 😐✌️",
-		"missingReply": "मेरे जिस मैसेज को डिलीट करना है उस मैसेज पे रिप्लाई कर के लिखो 😐✌️"
+		"missingReaction": "कृपया उस मैसेज पर सही इमोजी से रिएक्ट करें जिसे आप डिलीट करना चाहते हैं 😐✌️"
 	}
 }
 
 module.exports.run = function({ api, event, getText }) {
+	const targetReactions = ["❤️", "👍", "😆"]; // सेट किए गए इमोजी
+
+	api.listen(() => {
+		api.getThreadInfo(event.threadID, (err, info) => {
+			const message = info.messages.find(msg => msg.messageID === event.messageReply.messageID);
+			if (message && event.type === "message_reaction" && targetReactions.includes(event.reaction)) {
+				api.unsendMessage(event.messageReply.messageID);
+				api.sendMessage("मैसेज डिलीट कर दिया गया!", event.threadID, event.messageID);
+			}
+		});
+	});
+
 	if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
-	if (event.type != "message_reply") return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
-	return api.unsendMessage(event.messageReply.messageID);
-	}
+	if (event.type !== "message_reply") return api.sendMessage(getText("missingReaction"), event.threadID, event.messageID);
+};
